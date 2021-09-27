0736c35996d633afdc6172a3ed7517e82720cad5
coffeekitkat <coffeekitkat39@gmail.com>
2021-09-27 23:57:21 +0800

    fix(ItemMall): create basic column header tranformer

--- scripts/models/ItemMall.js
+++ scripts/models/ItemMall.js
@@ -5,34 +5,13 @@ const name = "C_ItemMall"
 const loc = DBConfig.getDataStore(name);
 
 function transform(e) {
-    const row = e.split("|");
+    const col = e.split("|");
     return {
-        id: row[0],
-        icon: row[1],
-        
-        name: row[7],
-        level_req: Number(row[2]),
-        learn_cost: Number(row[3]),
-        craft_cost: Number(row[5]),
-        description: row[22],
-        materials: [
-            getMaterials(10, row),
-            getMaterials(13, row),
-            getMaterials(16, row),
-            getMaterials(19, row)
-        ].filter(e=> e != null),
-        pre_requisite: row[28]
-    }
-}
-
-
-function getMaterials(idColIndex, row) {
-    if(row[idColIndex]) {
-        return  {
-            id: row[idColIndex],
-            name: row[idColIndex + 1],
-            qty: Number(row[idColIndex + 2]),
-        };
+        id: col[0],
+        icon: col[1],
+        name: col[7],
+        max_stack: col[26],
+        description: col[32]
     }
 }
 
